7b238bcab5ccb0ecb7dc2743ce30c1afe98713d4
DJWADE <g00208908@stu.raritanval.edu>
2025-04-16 13:34:12 -0400

    fixed?

--- Assets/01-Professor/ChangeDirection.cs
+++ Assets/01-Professor/ChangeDirection.cs
@@ -5,41 +5,47 @@ using UnityEngine;
 
 public class ChangeDirection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-     
-    myAnimator = this.gameObject.GetComponent<Animator>();
-
-
     private Animator myAnimator;
     private float horizDir;
     private float vertDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find the animator
+        myAnimator = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        //read from the keyboard
+        horizDir = Input.GetAxis("Horizontal");
+        vertDir = Input.GetAxis("Vertical");
 
-{
-    horizDir = Input.GetAxis("Horizontal")
-    vertDir = Input.GetAxis("Vertical")
-
+        // use the input to send a message to the animator
         if (horizDir < 0)
-    {
-        myAnimator.SetInteger("direction", 1)
-    }
-    else if (horizDir > 0)
-    {
-        myAnimator.SetInterger("direction", 3)
-    }
-    
-    else if (vertDir < 0)
-    {
-        myAnimator.SetInterger("direction", 2)
-    }
-    
-    else if (vertDir > 0)
-    { 
-        myAnimator.SetInterger("direction", 0)
+        {
+            myAnimator.SetInteger("direction", 1);
+        }
+        else if (horizDir > 0)
+        {
+            myAnimator.SetInteger("direction", 3);
+        }
+        else if (vertDir > 0)
+        {
+            myAnimator.SetInteger("direction", 2);
+        }
+        else if (vertDir < 0)
+        {
+            myAnimator.SetInteger("direction", 0);
+        }
+        // let professor stay in direction last chosen
+        //else  // this would be the idle state - go North by default
+        //{
+        //    myAnimator.SetInteger("direction", 0);
+        //}
+
     }
 }
